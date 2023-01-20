5b38692884d0a8e03e0c9b71e095f05ed3d17d75
Melchor <mel@ctav.es>
2023-01-20 17:25:49 +0100

    load local y server, activacion de GET

---
+++ example/leedir.php
@@ -0,0 +1,51 @@
+<?php
+
+	function human_filesize($bytes, $decimals = 2) {
+	  $sz = 'BKMGTP';
+	  $factor = floor((strlen($bytes) - 1) / 3);
+	  return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+	}
+
+	//~ echo print_r ( $_GET). "<br>";
+	//~ echo print_r ( $_GET[0]). "<br>";
+	//~ echo array_keys($_GET)[0]. "<br>";
+	
+	// $archIFC = array_keys($_GET)[0];
+	// $title = isset ($archIFC) ? $archIFC : 'imip';
+	
+	// if (isset ($archIFC) ) {
+	// 	echo "	<script>	var archIFC		= '".$archIFC."';	</script>		";
+	// 	echo "
+	// 		<aside class='side-menu' id='side-menu-left'></aside>
+	// 		<div id='viewer-container'></div>
+	// 		<div id='loading-overlay' class='loading-overlay hidden'>
+	// 		<h1 id='loading-progress' class='loading-progress'></h1>
+	// 		</div>
+	// 		<script async src='files/opencv.js' type='text/javascript'></script>
+	// 		<script type='module' src='./build/main.js'></script>
+	// 	";
+	// }
+	// else {
+		//~ echo "No hay GET";
+		// echo " <div class='contenedor'>";
+		// echo " <div class='logo'><img src='img/logoIMIP.png' alt='logo IMIP'></div>";
+		// echo " <div class='table'>";
+		foreach (glob("models/*.ifc") as $nombre_fichero) {
+			$sinRuta = substr($nombre_fichero, strrpos($nombre_fichero, "/")+1, strlen($nombre_fichero));
+			$sinExtension = substr($sinRuta, 0, strrpos($sinRuta, "."));
+			// echo " 	<div class='row' onClick='parent.open(`index.php?$sinExtension`)' value='click here to visit home page'>
+			// 			<div class='cell colIni' >$sinExtension </div>
+			// 			<div class='cell textPeque'>" . human_filesize(filesize($nombre_fichero),2) . "</div>
+			// 			<div class='cell textPeque' >" . date('Y-m-d G:i:s', filemtime($nombre_fichero)) . "</div>
+			// 		</div>
+			// 	";
+			$datosActuales[] = [$sinExtension, human_filesize(filesize($nombre_fichero),2),date('Y-m-d G:i:s', filemtime($nombre_fichero))]  ;
+		}
+		$out = $datosActuales;
+		echo json_encode( $out);
+
+		
+
+	// }
+
+?>
